1c62d5bafdabe4e8ac7c5c834b9eab2834cc22b6
tonymarsh <tonymarsh0331@gmail.com>
2022-12-03 01:46:19 +0800

    :art: add more documents

--- spring_15_spring_mybatis/src/main/java/com/itheima/config/MybatisConfig.java
+++ spring_15_spring_mybatis/src/main/java/com/itheima/config/MybatisConfig.java
@@ -10,6 +10,7 @@ public class MybatisConfig {
     //定义bean，SqlSessionFactoryBean，用于产生SqlSessionFactory对象
     @Bean
     public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource){
+        // ssfb stands for SqlSessionFactoryBean , 该类由Spring提供，封装了创建SqlSessionFactory的细节，我们只需要配置重要的属性即可
         SqlSessionFactoryBean ssfb = new SqlSessionFactoryBean();
         ssfb.setTypeAliasesPackage("com.itheima.domain");
         ssfb.setDataSource(dataSource);
@@ -18,8 +19,9 @@ public class MybatisConfig {
     //定义bean，返回MapperScannerConfigurer对象
     @Bean
     public MapperScannerConfigurer mapperScannerConfigurer(){
+        // 同样的，该类也是Spring提供的用于简化创建配置对象的类，我们只需要关注配置重要的属性即可
         MapperScannerConfigurer msc = new MapperScannerConfigurer();
-        msc.setBasePackage("com.itheima.dao");
+        msc.setBasePackage("com.itheima.dao"); // Mapper接口所在的包
         return msc;
     }
 }
